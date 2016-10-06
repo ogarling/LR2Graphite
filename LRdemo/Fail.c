bdaff23cdd956551711ee82c0cbb76cb9a3da79b
Okke Garling <okke.garling@perfcon.nl>
2016-10-06 11:41:15 +0200

    added failed transaction and modified default run logic

--- LRdemo/Fail.c
+++ LRdemo/Fail.c
@@ -1,8 +1,10 @@
-Action()
+Fail()
 {
 	lr_start_transaction(lr_eval_string("Trans{rndTrans}"));
 		lr_think_time(2);
-	lr_end_transaction(lr_eval_string("Trans{rndTrans}"), LR_AUTO);
+	lr_end_transaction(lr_eval_string("Trans{rndTrans}"), LR_FAIL);
+
+	lr_think_time(2);
 
 	return 0;
 }
